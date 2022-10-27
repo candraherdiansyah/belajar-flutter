52489f3b6daeb0c0edaf2e0f25b2b79326e99dc2
candra <candraherdiansyah14@gmail.com>
2022-10-27 09:37:53 +0700

    auth api

---
+++ lib/network/api.dart
@@ -0,0 +1,7 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Network{
+  final String _baseUrl = 'http://192.168.2.201/'
+}
